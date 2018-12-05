2959cd454caea477b18fe96b7956767c1e975e22
markus <markus@4eyes.ch>
2018-12-05 02:23:25 +0100

    implement list of certificates

--- src/components/Seller.vue
+++ src/components/Seller.vue
@@ -14,7 +14,6 @@
             </v-flex>
 
 
-
             <v-flex xs12>
                 <h2 class="display-2 font-weight-bold mb-3">Sell certificates</h2>
                 <v-alert
@@ -103,17 +102,16 @@
 
             <v-flex xs12>
                 <v-data-table
-                        :headers="seller.certificates.headers"
-                        :items="seller.certificates.data"
+                        :headers="certificates.headers"
+                        :items="certificates.data"
                         hide-actions
                         class="elevation-1"
                 >
                     <template slot="items" slot-scope="props">
-                        <td class="text-xs-left">{{ props.item.unipi.name }}</td>
+                        <td class="text-xs-left">{{ props.item.unipi }}</td>
                         <td class="text-xs-left">{{ props.item.id }}</td>
-                        <td class="text-xs-left">{{ props.item.price }}</td>
-                        <td class="text-xs-left">{{ props.item.generated }}</td>
-                        <td class="text-xs-left">{{ props.item.sold }}</td>
+                        <td class="text-xs-left">{{ props.item.minimalPrice }}</td>
+                        <td class="text-xs-left">{{ props.item.sellingPrice }}</td>
                     </template>
                 </v-data-table>
             </v-flex>
@@ -176,88 +174,20 @@
                     }
                 ]
             },
-            consumers: {
-                data: [
-                    {
-                        id: "ewb_kohlestromallee6bern_grid_import3p",
-                        name: "Kohlestromallee 6"
-                    },
-                    {
-                        id: "ewb_kohlestromallee8bern_grid_import3p",
-                        name: "Kohlestromallee 8"
-                    },
-                    {
-                        id: "ewb_kohlestromallee9bern_grid_import3p",
-                        name: "Kohlestromallee 9"
-                    },
-                    {
-                        id: "ewb_kohlestromallee10bern_grid_import3p",
-                        name: "Kohlestromallee 10"
-                    },
-                    {
-                        id: "ewb_kohlestromallee11bern_grid_import3p",
-                        name: "Kohlestromallee 11"
-                    },
-                    {
-                        id: "ewb_kohlestromallee13bern_grid_import3p",
-                        name: "Kohlestromallee 13"
-                    }
-                ]
-            },
-            seller: {
-                certificates: {
-                    headers: [
-                        {text: 'Unipi', value: 'unipi'},
-                        {text: 'Id', value: 'id'},
-                        {text: 'Price', value: 'price'},
-                        {text: 'Generated', value: 'generated'},
-                        {text: 'Sold', value: 'sold'},
-                    ],
-                    data: [
-                        {
-                            id: '932828fae3ef07b7fe8c7300b061f765',
-                            price: '0.02',
-                            generated: '2018-12-03 22:41',
-                            sold: '',
-                            unipi:
-                                {
-                                    name: 'UniPi1 - Hauptstrasse 342'
-                                }
-                        },
-                        {
-                            id: 'afe9bfdea0c7ea66d638e56e158b192b',
-                            price: '0.03',
-                            generated: '2018-12-02 22:41',
-                            sold: '',
-                            unipi:
-                                {
-                                    name: 'UniPi2 - Hauptstrasse 344'
-                                }
-                        },
-                        {
-                            id: '932828fae3ef07b7fe8c7300b061f745',
-                            price: '0.04',
-                            generated: '2018-12-01 22:41',
-                            sold: '2018-12-02 22:41',
-                            unipi:
-                                {
-                                    name: 'UniPi2 - Hauptstrasse 344'
-                                }
-                        },
-                        {
-                            id: '28898a6cf5301fc6aa649355e089ebe4',
-                            price: '0.01',
-                            generated: '2018-11-30 22:41',
-                            sold: '2018-11-30 23:41',
-                            unipi:
-                                {
-                                    name: 'UniPi1 - Hauptstrasse 342'
-                                }
-                        }
-                    ]
-                }
+            certificates: {
+                headers: [
+                    {text: 'Unipi', value: 'unipi'},
+                    {text: 'Id', value: 'id'},
+                    {text: 'Minimal price', value: 'minimalPrice'},
+                    {text: 'Price sold', value: 'sellingPrice'},
+                ],
+                data: []
             }
         }),
+        mounted() {
+            this.getMyCertificates();
+            this.getPrice();
+        },
         methods: {
             setSellingPrice() {
                 let postBody = {
@@ -314,6 +244,28 @@
                     .catch(e => {
                         console.log(e);
                     });
+            },
+            getMyCertificates() {
+                let that = this;
+                let dataLen = this.producers.data.length;
+                let i = 0;
+                for (i = 0; i < dataLen; i++) {
+                    axios({
+                        method: 'get',
+                        url: 'http://localhost:8000/certificate/getAllByUniPi/' + this.producers.data[i].id,
+                    })
+                        .then(function (response) {
+                            if (response.data.length > 0) {
+                                // that.certificates.data.concat(response.data);
+                                that.certificates.data = that.certificates.data.concat(response.data);
+                            }
+
+                        })
+                        .catch(e => {
+                            console.log(e);
+                        });
+                }
+
             }
         }
     }
